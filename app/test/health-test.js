44715aa15a3ac126b8224d22ab741c907344ce7e
PrekshaYadav <113851464+PrekshaYadav@users.noreply.github.com>
2024-02-12 16:57:02 -0500

    demo

--- app/test/health-test.js
+++ app/test/health-test.js
@@ -4,7 +4,7 @@ const assert = require('assert');
 describe('Basic Addition Test', function() {
   it('Should return the result of 4 + 4', function(done) {
     const result = 4 + 4;
-    assert.strictEqual(result, 8);
+    assert.strictEqual(result, 10);
     done();
   });
 });
